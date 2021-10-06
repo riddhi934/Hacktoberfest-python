04fa98350bd85ce668b8255f42fe49d783a0bac8
Devnarayan Patel <52664716+devnarayanp02@users.noreply.github.com>
2021-10-06 11:06:51 +0530

    Create Replace Blank Space with Hyphen in a String.py

---
+++ Replace Blank Space with Hyphen in a String.py
@@ -0,0 +1,8 @@
+# Python program to Replace Blank Space with Hyphen in a String
+ 
+str1 = input("Please Enter your Own String : ")
+
+str2 = str1.replace(' ', '_')
+
+print("Original String :  ", str1)
+print("Modified String :  ", str2)
